352350a9a6c312e1d329d1c3ceb33fb61e8011c2
Hilton1 <josehilton02@gmail.com>
2020-03-25 20:27:23 -0300

    Back-end da aplicação

---
+++ backend/anotacoes.js
@@ -0,0 +1,34 @@
+/**
+* Rota / Recurso
+*/
+
+/**
+* Métodos HTTP:
+* 
+* GET - Buscar/listar uma informação do back-end
+* POST - Criar uma informação no back-end
+* PUT - Alterar uma informação no back-end
+* DELETE - Deletar uma informação no back-end
+*/
+
+/**
+ * Tipos de parâmetros:
+ * 
+ * Query Params - Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
+ * Route Params - Parâmetros utilizados para identificar recursos
+ * Request Body - Corpo do requisição, utilizado para criar ou alterar recursos
+ */
+
+/** Bancos de Dados:
+ * 
+ *  SQL - MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server 
+ * 
+ *             Pesquisar por Knex pra fazer um banco em otopatamar
+ * 
+ *  NoSQL - MongoDB, CouchDB, etc
+ */
+
+/**
+ * Driver - SELECT * FROM users
+ * Query Builder - table('users').select('*').where()
+ */
